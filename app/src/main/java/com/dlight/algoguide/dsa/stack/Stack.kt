162a34fc7ed0a129449cb825125253b64c6f885b
Prashant <pr.prashant0786@gmail.com>
2022-10-04 05:02:06 +0530

    Stack Code Added

--- app/src/main/java/com/dlight/algoguide/dsa/stack/Stack.kt
+++ app/src/main/java/com/dlight/algoguide/dsa/stack/Stack.kt
@@ -1,4 +1,58 @@
 package com.dlight.algoguide.dsa.stack
 
-class Stack {
+import java.util.ArrayList
+
+class Stack<E>(var sz: Int) {
+    private var arrayList: ArrayList<E> = ArrayList()
+    fun push(item: E): E? {
+        if (isFull) {
+            println("STACK IS FULL")
+            return null
+        }
+        arrayList.add(item)
+        return item
+    }
+
+    fun pop(): E? {
+        if (isEmpty) {
+            return null
+        }
+        val len = size()
+        val obj: E? = peek()
+        arrayList.removeAt(len - 1)
+        return obj
+    }
+
+    fun peek(): E? {
+        val len = size()
+        if (isEmpty) {
+            return null
+        }
+        return arrayList[len - 1]
+    }
+
+    val isFull: Boolean
+        get() = sz == arrayList.size
+    val isEmpty: Boolean
+        get() = arrayList.size == 0
+
+    private fun size(): Int {
+        return arrayList.size
+    }
+}
+
+fun main() {
+    val st  = Stack<Int>(5);
+    st.isEmpty
+    st.isFull
+    println(st.peek())
+    println(st.pop())
+    println(st.pop())
+    println(st.pop())
+    println(st.pop())
+    println(st.pop())
+    println(st.pop())
+    println(st.pop())
+    println(st.pop())
+    println(st.pop())
 }
